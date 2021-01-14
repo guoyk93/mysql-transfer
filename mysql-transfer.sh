290407afc52c748ac84c45c6782c24e501a36653
Guo Y.K <hi@guoyk.net>
2021-01-14 18:25:28 +0800

    quote database name

--- mysql-transfer.sh
+++ mysql-transfer.sh
@@ -18,7 +18,7 @@ MYSQL_DST_PASS="${MYSQL_DST_PASS}"
 MYSQL_DST_NAME="${MYSQL_DST_NAME}"
 
 cat <<-EOF | mysql -h "${MYSQL_DST_HOST}" "-u${MYSQL_DST_USER}" "-p${MYSQL_DST_PASS}" -P "${MYSQL_DST_PORT}"
-create database if not exists ${MYSQL_DST_NAME} default character set utf8mb4 collate utf8mb4_general_ci;
+create database if not exists \`${MYSQL_DST_NAME}\` default character set utf8mb4 collate utf8mb4_general_ci;
 EOF
 
 mysqldump -h "${MYSQL_SRC_HOST}" "-u${MYSQL_SRC_USER}" "-p${MYSQL_SRC_PASS}" -P "${MYSQL_SRC_PORT}" ${MYSQL_DUMP_OPTS} "${MYSQL_SRC_NAME}" | mysql -h "${MYSQL_DST_HOST}" "-u${MYSQL_DST_USER}" "-p${MYSQL_DST_PASS}" -P "${MYSQL_DST_PORT}" ${MYSQL_LOAD_OPTS} "${MYSQL_DST_NAME}"
